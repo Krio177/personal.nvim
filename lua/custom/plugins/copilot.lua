17b7277623ef49bfa3265ab6517d18b87a95291c
Teigler Tibor <ttibi17@gmail.com>
2025-06-23 23:08:30 +0200

    Copilot next suggest

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -6,6 +6,9 @@ return {
     config = function()
       vim.g.copilot_no_tab_map = true -- Tiltsa le a Tab alapértelmezett hozzárendelését
       vim.g.copilot_assume_mapped = true
+      
+      vim.api.nvim_set_keymap('i', '<silent> <C-S-n>', '<Plug>(copilot-next)', {})
+      vim.api.nvim_set_keymap('i', '<silent> <C-S-p>', '<Plug>(copilot-previous)', {})
     end,
   },
   {
