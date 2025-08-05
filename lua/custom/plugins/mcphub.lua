806eec93b125a81a800a3b7e6fdc535934705559
Teigler Tibor <ttibi17@gmail.com>
2025-08-05 20:10:49 +0200

    MCP disable

--- lua/custom/plugins/mcphub.lua
+++ lua/custom/plugins/mcphub.lua
@@ -1,14 +1,14 @@
 return {
-  'ravitemer/mcphub.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  build = 'npm install -g mcp-hub@latest', -- Globális telepítés
-  config = function()
-    require('mcphub').setup {
-      -- Itt testreszabhatod a beállításokat, pl. port, log, stb.
-      -- port = 3000,
-      -- config = vim.fn.expand("~/.config/nvim/mcpservers.json"),
-    }
-  end,
+  -- 'ravitemer/mcphub.nvim',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  -- },
+  -- build = 'npm install -g mcp-hub@latest', -- Globális telepítés
+  -- config = function()
+  --   require('mcphub').setup {
+  --     -- Itt testreszabhatod a beállításokat, pl. port, log, stb.
+  --     -- port = 3000,
+  --     -- config = vim.fn.expand("~/.config/nvim/mcpservers.json"),
+  --   }
+  -- end,
 }
